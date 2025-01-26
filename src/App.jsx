8e5a408c2af6142a42874455ca5abc1f46ee4e6c
amanmittal06 <amanagrawal0139@gmail.com>
2025-01-26 14:13:15 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,13 @@ function App() {
     setCount(count+1);
   }
 
+  function reset() {
+    setCount(0);
+    setTotalRounds(0);
+    localStorage.setItem('count', 0);
+    localStorage.setItem('totalRounds', 0);
+  }
+
   useEffect(() => {
     const savedCount = localStorage.getItem('count');
     const savedTotalRounds = localStorage.getItem('totalRounds');
@@ -40,14 +47,16 @@ function App() {
     
 
     <center className='container'>
+      <div className='mantra' >हरे कृष्ण हरे कृष्ण<br/>कृष्ण कृष्ण हरे हरे<br/>हरे राम हरे राम<br/>राम राम हरे हरे</div>
       <center className='box'>
+         
          <div className='count'>
           <button disabled className='countButton'>-</button>
           <div>{count}</div>
           <button className='countButton' onClick={()=>increseCount()}>+</button>
          </div>
          <div>Total rounds: {totalRounds}</div>
-         <button className='resetButton'>Reset</button>
+         <button className='resetButton' onClick={()=>reset()}>Reset</button>
       </center>
     </center>
   )
